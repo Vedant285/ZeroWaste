2c7db12a00d34d6469b8dc4b2f46ec70870de83e
Vedant Shukla <91669901+Vedant285@users.noreply.github.com>
2024-02-10 19:43:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,18 +6,22 @@ st.set_page_config('ZeroWaste', ':cook:')
 with open('Instructions.txt') as f:
     Instruction = f.read()
     
+if 'food items' not in st.session_state:
+    st.session_state['food items'] = []
+
+food_recognizer = FoodRecognizer(st.secrets['clarifai_api']['PAT'])
 
 def suggest(dish, num_of_recipes):  # Function to suggest dishes from the ingredients given to it 
     prompt = f'''
-    Suggest me {num_of_recipes} recipes from {dish}
-    Should follow the following template:
-    **Dish Name**
+    Provide {num_of_rcps} recipe suggestions for these food items: {food_tags}
+    Write in this structure:
+    **dish name**
     1. 
-    2.
-    3.
-    4.
+    2. 
+    3. 
+    Don't write ingredients.
     '''
-    return bot(prompt)
+    return "bot(prompt)"
 
 
 st.title("ZeroWaste")   #title of project
@@ -35,10 +39,27 @@ with tab2:
         capture = st.camera_input("Take the image of food ingredients or vegetable")
         if capture:
             img = Image.open(capture)
-        else:
-            st.write("We are unable to detect. Can you please type the name if you know, otherwise try to capture again.")
+            top_pred = list(food_recognizer.recognize(img).items())[0]
+            if top_pred[1] > 0.4:
+                if top_pred[0] not in st.session_state['food items']:
+                    st.session_state['food items'].append(top_pred[0])
+                    st.success(f'{top_pred[0]} detected with conf. of {top_pred[1]}')
+                else:
+                    st.info(f'{top_pred[0]} already in list.')
+            else:
+                st.error('AI cannot recognize item!')
+        st.write('Food Items Recognized:')
+        for item in st.session_state['food items']:
+            st.markdown(f'- {item}')
+
+        if st.button('Clear'):
+            st.session_state['food items'] = []
+
+        if st.button('Suggest') and st.session_state['food items']:
+            st.write(suggest(num_of_rcps, st.session_state['food items']))
+        
             
     else:
         text = st.text_input("Enter the name of ingredients ex: mango, banana, orange etc")
-        if text and num_of_recipes:
+        if st.button('Suggest') and num_of_recipes:
             st.write(suggest(text, num_of_recipes))
