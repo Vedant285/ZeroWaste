c552c3bcd02f87133e83259919277c351dce7e41
Vedant Shukla <91669901+Vedant285@users.noreply.github.com>
2024-02-10 17:22:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,13 @@ from ClarifaiAPI import FoodRecognizer
 
 st.set_page_config('ZeroWaste', ':cook:') 
 with open('Instructions.txt') as f:
-    Instruction=f.read()
+    Instruction = f.read()
     
 
-
-def suggest(dish,num_of_Recipes):  # Function to suggest dishes from the ingrediants given to it 
-    prompt=f'''
-    Suggest me {num_of_Recipes} recepies from {dish}
-    SHould follow the following template:
+def suggest(dish, num_of_recipes):  # Function to suggest dishes from the ingredients given to it 
+    prompt = f'''
+    Suggest me {num_of_recipes} recipes from {dish}
+    Should follow the following template:
     **Dish Name**
     1. 
     2.
@@ -22,24 +21,24 @@ def suggest(dish,num_of_Recipes):  # Function to suggest dishes from the ingredi
 
 
 st.title("ZeroWaste")   #title of project
-tab1,tab2=st.tabs(['Instructions',"Food Dish"])    #2 tabs 1st for Instructions  and 2nd is for Dishes 
+tab1, tab2 = st.tabs(['Instructions', "Food Dish"])    #2 tabs 1st for Instructions  and 2nd is for Dishes 
 
 
 with tab1:
     st.markdown(Instruction)
-    st.image('logo.png',width=400)
+    st.image('logo.png', width=400)
     
 with tab2:
-    input=st.radio("Choose how you want to tell us about the ingredients :",['Camera','Text'])
-    num_of_Recipes=st.number_input("Select how many dishes you want ",1,4,1,1) #1,4 1 1 means start the count from 1, go upto 4, start the preview of count 1 from and after each increment , increment the number by 1.
-    if input=='Camera':
-        capture=st.camera_input("Take the image of food ingredients or vegitable")
+    input_choice = st.radio("Choose how you want to tell us about the ingredients:", ['Camera', 'Text'])
+    num_of_recipes = st.number_input("Select how many dishes you want", 1, 4, 1, 1) # 1,4 1 1 means start the count from 1, go up to 4, start the preview of count 1 from and after each increment, increment the number by 1.
+    if input_choice == 'Camera':
+        capture = st.camera_input("Take the image of food ingredients or vegetable")
         if capture:
-            img=Image.open(capture)
+            img = Image.open(capture)
         else:
-            st.write("We are unable to Detect can you please type the name if you know, otherwise try to capture again.")
+            st.write("We are unable to detect. Can you please type the name if you know, otherwise try to capture again.")
             
     else:
-        text=st.text_input("Enter the name of ingredients ex: mango, banana, orange etc")
-        if text and num_of_Recipes:
-            st.write(suggest(text,num_of_Recipes))
+        text = st.text_input("Enter the name of ingredients ex: mango, banana, orange etc")
+        if text and num_of_recipes:
+            st.write(suggest(text, num_of_recipes))
