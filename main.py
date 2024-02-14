43d518e8956a297661bc7cbcd36ac8d0277f87cd
Vedant Shukla <91669901+Vedant285@users.noreply.github.com>
2024-02-14 20:26:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -32,9 +32,9 @@ llm_prompt = ChatPromptTemplate.from_messages([
 ])
 
 # Initialize Objects
-clarifai  = Clarifai(pat=st.secrets["PAT"], user_id='openai', app_id='chat-completion', model_id='GPT-4')
+clarifai  = Clarifai(pat=st.secrets["PAT"],  model_id='GPT-4')
 
-langchain_model  = LLMChain(llm=clarifai, prompt=llm_prompt, verbose=True)
+langchain_model  = LLMChain(llm=clarifai)
 
 
 st.title("ZeroWaste")   #title of project
